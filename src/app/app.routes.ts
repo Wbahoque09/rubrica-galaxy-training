9c0f112add688a1f716cc51e4e7ab4e73ae4ef3e
William <william.bahoque@silverapp.co>
2025-09-12 16:48:36 -0500

    feat: :sparkles: Rutas & TailwndCSS
    Se empieza creando las respectivas rutas del proyecto y componentes, se agrega tailwind al proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./shared/home.route').then(r=>r.home_routes)
+  }
+];
