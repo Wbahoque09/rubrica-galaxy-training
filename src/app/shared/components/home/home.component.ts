31fdec7d1def9d31d9ced6676f2d2dcdf5f8fb86
William <william.bahoque@silverapp.co>
2025-09-15 16:30:16 -0500

    feat: :sparkles: Navbar
    Se añade el navbar y se hace la maquetacion

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
