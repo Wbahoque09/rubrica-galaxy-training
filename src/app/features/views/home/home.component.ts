5ae7179d3ad78b2c079b9e6a3750fd5ad355360e
William <william.bahoque@silverapp.co>
2025-09-22 17:01:29 -0500

    feat: :sparkles: Table
    Se hace el diseño de la tabla y unas pruebas con data fake para modelar

--- src/app/features/views/home/home.component.ts
+++ src/app/features/views/home/home.component.ts
@@ -18,15 +18,66 @@ export class HomeComponent implements OnInit {
   }
 
   getProduct() {
-    this.productService.getAllProducts().subscribe(
-      {
-        next: (response) => {
-          console.log(response);
-        },
-        error: (err) => {
-          console.warn(err);
-        }
-      }
-    )
+    this.productService.getAllProducts().subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.warn(err);
+      },
+    });
   }
+
+  columns = [
+    { header: 'ID', field: 'id' },
+    { header: 'Nombre', field: 'name' },
+    { header: 'Correo', field: 'email' },
+    { header: 'Rol', field: 'role' },
+    { header: 'Estado', field: 'status' },
+  ];
+
+  rows = [
+    {
+      id: 1,
+      name: 'Juan',
+      email: 'juan@mail.com',
+      role: 'Admin',
+      status: 'Activo',
+    },
+    {
+      id: 2,
+      name: 'Ana',
+      email: 'ana@mail.com',
+      role: 'User',
+      status: 'Activo',
+    },
+    {
+      id: 3,
+      name: 'Luis',
+      email: 'luis@mail.com',
+      role: 'User',
+      status: 'Inactivo',
+    },
+    {
+      id: 4,
+      name: 'Maria',
+      email: 'maria@mail.com',
+      role: 'Admin',
+      status: 'Activo',
+    },
+    {
+      id: 5,
+      name: 'Pedro',
+      email: 'pedro@mail.com',
+      role: 'User',
+      status: 'Activo',
+    },
+    {
+      id: 6,
+      name: 'Carla',
+      email: 'carla@mail.com',
+      role: 'User',
+      status: 'Activo',
+    },
+  ];
 }
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,9 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, InputSignal, Signal, signal } from '@angular/core';
 
+interface Headers {
+  header: string[];
+  field: string[];
+}
 @Component({
   selector: 'table',
   imports: [],
   templateUrl: './table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent { }
+export class TableComponent {
+  // @Input() columns: { header: string; field: string }[] = [];
+  // @Input() data: any[] = [];
+  // value = input.required<Headers>;
+  protected readonly value: InputSignal<Headers> = input.required();
+  data = input.required<string[]>;
+
+  // Paginación
+  protected readonly currentPage = signal(1);
+  protected readonly itemsPerPage = signal(5);
+  protected readonly arrayHeaders: Signal<string> = computed(() => `${this.value().header}`);
+
+  get paginatedData() {
+    const start = (this.currentPage() - 1) * this.itemsPerPage();
+    return start;
+    // return this.data.slice(start, start + this.itemsPerPage);
+  }
+
+  get totalPages() {
+    return Math.ceil(this.data.length / this.itemsPerPage());
+  }
+
+  protected changePage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage.set(page);
+    }
+  }
+}
